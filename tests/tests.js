fa0c6aaa0519cc566ea1d706a81d4d14a351de02
Keith March Mistler <kmistler@gmail.com>
2021-05-28 12:49:29 -0400

    set up test file

---
+++ tests/tests.js
@@ -0,0 +1,17 @@
+/* eslint-disable indent */
+let describe = require('mocha').describe
+let it = require('mocha').it
+let calculations = require('../calculations')
+let expect = require('chai').expect
+
+describe('calculations', () => {
+  describe('retirementBalance', () => {
+    it('returns the sum of the numbers in the array provided', () => { 
+      // setup test data
+
+      // call the function with the test data
+
+      // asserts what should be true
+    })
+  })
+})
